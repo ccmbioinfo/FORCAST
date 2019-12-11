980715f819991272933fe8a2cb66d45de4990d3e
helrick <hillary.elrick@sickkids.ca>
2019-12-11 21:23:42 +0000

    minor tweak to landing page behaviour

--- landing-page/js/functions.js
+++ landing-page/js/functions.js
@@ -6,5 +6,5 @@ function navigateToGenome() {
 	genome = $("#genomeSelector option:selected").val();	
 	jbrowseURL = window.location.protocol + "//" + window.location.host;
 	jbrowseURL += "/jbrowse/index.html?data=data." + genome;
-	window.open(jbrowseURL);
+	window.open(jbrowseURL,"_self");
 }
