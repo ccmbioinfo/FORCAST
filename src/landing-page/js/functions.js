4cd5711b65f03dcc3101e263c8409d4289512dcd
Kevin Mo <kevin.hy.mo@gmail.com>
2023-08-18 15:57:33 -0400

    Format HTML, CSS, and JavaScript code using Prettier and add requirements-dev.txt for development dependencies (for black and isort)

--- src/landing-page/js/functions.js
+++ src/landing-page/js/functions.js
@@ -1,12 +1,12 @@
-$(document).ready(function() {
-    $('#genomeSelector').select2();
+$(document).ready(function () {
+  $("#genomeSelector").select2();
 
-	$.post("../primer-design/web/ajaxCalls/fetchInstalledGenomes.py", function(data) {
-		$("#genomeSelector").html(data);
-	});
+  $.post("../primer-design/web/ajaxCalls/fetchInstalledGenomes.py", function (data) {
+    $("#genomeSelector").html(data);
+  });
 
-	$("#goButton").click(function () {
-		var genome = $("#genomeSelector option:selected").val();
-		location = "/jbrowse/index.html?data=data/" + genome;
-	});
+  $("#goButton").click(function () {
+    var genome = $("#genomeSelector option:selected").val();
+    location = "/jbrowse/index.html?data=data/" + genome;
+  });
 });
